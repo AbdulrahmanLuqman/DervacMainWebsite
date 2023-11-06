2f1302bd0b7cbc04647a86c1f74507140208d2c3
AbdulrahmanLuqman <fal528@dervac.com>
2023-11-06 09:52:20 +0100

    added pinia store

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,72 @@
 <template>
-    <div>
-        <p class="text-2xl text-black">Nav</p>
-    </div>
+    <header class="bg-[#FFFFFF] px-16 py-10">
+        <nav class="flex gap-20">
+            <div class="flex items-center h-fit">
+                <img class="w-[40px]" src="../assets/images/dervac_logo.png" alt="LOGO">
+                <p class="text-2xl font-bold text-[#224C75]">dervac<span class="text-[#DC2828]">hub</span></p>
+            </div>
+
+            <ul class="flex items-center gap-10 border-t h-[50px]">
+                <li class="cursor-pointer text-lg font-semibold text-[#224C75] hover:text-[#DC2828] hover:text-[#8D8EA3] hover:border-t-2 hover:border-[#DC2828] p-2 h-full">Home</li>
+                <li class="relative">
+                    <div class="flex items-center">
+                        <p class="font-semibold text-[#224C75]">Who We Are</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#929FB5" d="M8.12 9.29L12 13.17l3.88-3.88a.996.996 0 1 1 1.41 1.41l-4.59 4.59a.996.996 0 0 1-1.41 0L6.7 10.7a.996.996 0 0 1 0-1.41c.39-.38 1.03-.39 1.42 0z"/></svg>
+                    </div>
+
+                    <ul class="absolute bg-red-200 left-[-69px] shadow-xl w-[300px] rounded-xl">
+                        <li class="text-[#8D8EA3] cursor-pointer px-8 py-2">About</li>
+                        <li class="text-[#8D8EA3] cursor-pointer px-8 py-2">Mission, Vision and Values</li>
+                        <li class="text-[#8D8EA3] cursor-pointer px-8 py-2">Leadership Team</li>
+                        <li class="text-[#8D8EA3] cursor-pointer px-8 py-2">Why Choose Us</li>
+                        <li class="text-[#8D8EA3] cursor-pointer px-8 py-2">Locations</li>
+                        <li class="text-[#8D8EA3] cursor-pointer px-8 py-2">FAQ</li>
+                        <li class="text-[#8D8EA3] cursor-pointer px-8 py-2">Careers</li>
+                        <li class="text-[#8D8EA3] cursor-pointer px-8 py-2">Pricing and plans</li>
+                    </ul>
+                </li>
+                <!-- <li class="relative">
+                    <p>IT Solutions</p>
+                    <div class="absolute">
+                        <div>
+                            <p>IT Services</p>
+                            <ul>
+                                <li>Managed IT</li>
+                                <li>IT Support</li>
+                                <li>IT Consultancy</li>
+                                <li>Cloud Computing</li>
+                                <li>Cyber Security</li>
+                                <li>Custom Software</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <p>Industries</p>
+                            <ul>
+                                <li>Banking</li>
+                                <li>Capital Markets</li>
+                                <li>Enterprises Technology</li>
+                                <li>Manufacturing</li>
+                                <li>Healthcare</li>
+                                <li>Higher Education</li>
+                                <li>Logistics</li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li>Case Studies</li>
+                <li>IT Blog</li>
+                <li>Contact Us</li> -->
+            </ul>
+
+           <!-- <div>
+                <img src="../assets/images/messages.svg" alt="Chat Icon">
+                <div>
+                    <p>Have any questions?</p>
+                    <p>Free: <span><a href="tel:09016461891">09016461891</a></span></p>
+                </div>
+            </div> -->
+        </nav>
+    </header>
 </template>
 
 <script>
