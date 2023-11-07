1630b5a5c20792af8ea258948780c733ed11d619
AbdulrahmanLuqman <fal528@dervac.com>
2023-11-07 07:23:02 +0100

    added hamburger icon

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -136,6 +136,26 @@
             </div>
         </nav>
     </header>
+
+    <header class="bg-[#FFFFFF] py-10 lg:hidden block">
+        <nav class="flex items-center justify-between px-16">
+            <div class="flex items-center h-fit">
+                <img class="w-[40px]" src="../assets/images/dervac_logo.png" alt="LOGO">
+                <p class="text-4xl font-bold text-[#224C75] font-[Bimini]">dervac</p>
+            </div>
+
+            <div class="flex gap-2">
+                <img class="w-[40px] cursor-pointer" src="../assets/images/messages.svg" alt="Chat Icon">
+
+                <div class="">
+                    <p class="font-semibold text-[#8D8EA3]">Have any questions?</p>
+                    <p class="text-[#8D8EA3] font-semibold">Free <span class="text-[#224C75] underline"><a>09016461891</a></span></p>
+                </div>
+            </div>
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="M4 7h3m13 0h-9m9 10h-3M4 17h9m-9-5h16"/></svg>
+        </nav>
+    </header>
 </template>
 
 <script>
