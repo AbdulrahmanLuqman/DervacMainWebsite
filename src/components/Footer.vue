54c188a7518bcc6e07489cc665a894023ef778f9
fawwazmak <fawwazmakinde47@gmail.com>
2023-11-08 21:42:01 +0100

    done with responsiveness

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -55,7 +55,7 @@
         </div>
 
         <div>
-            <p class="text-[#8D8EA3] text-center py-10 border-t">Copyright &copy; {{ year }} Dervac. Desingned and Developed by <a href="heb.dervac.com" class="text-[#253858] hover:text-[#DC2828]">DervacHub Team</a></p>
+            <p class="text-[#8D8EA3] text-center py-10 border-t">Copyright &copy; {{ year }} <span class="font-[Bimini]">Dervac</span>. Designed and Developed by <a href="heb.dervac.com" class="text-[#253858] hover:text-[#DC2828]"> <span class="font-[Bimini]">DervacHub Team</span></a></p>
         </div>
     </footer>
 </template>
@@ -74,5 +74,12 @@
 </script>
 
 <style scoped>
-
+@font-face {
+        font-family: 'Bimini';
+        src: url('../../assets/bimini/BiminiNormal.ttf') format('truetype');
+             /* url('./bimini/BIMINI__.TTF') format('TTF'); */
+        /* You can add more formats if needed */
+        font-weight: normal;
+        font-style: normal;
+}
 </style>
--- src/components/LandingPage/TechCompanySection.vue
+++ src/components/LandingPage/TechCompanySection.vue
@@ -2,13 +2,13 @@
 
 </script>
 
-
+<!-- min-[895px]:hidden -->
 <template>
 <div class="thisDiv">
-    <div class="px-20 text-center text-white flex flex-col space-y-3 items-center w-[75%] mx-auto md:pt-[150px] pt-0">
-        <p class="text-[#dc2828] text-[16px] font-semibold">WE ARE TECH COMPANY</p>
+    <div class="px-20 text-center sm:text-white flex flex-col space-y-3 items-center w-[75%] mx-auto md:pt-[150px] sm:pt-[60px] pt-6">
+        <p class="text-[#dc2828] sm:text-[16px] text-[10px] font-semibold">WE ARE TECH COMPANY</p>
 
-        <p class="text-[35px] font-bold">Turning Difficulties into Technological Victories</p>
+        <p class="md:text-[35px] sm:text-[25px] text-[15px] font-bold">Turning Difficulties into Technological Victories</p>
 
         <p>At our core, we specialize in converting complex challenges into resounding technological successes. We thrive on finding innovative solutions that not only overcome obstacles but also propel your business forward. Our team of experts is dedicated to harnessing the power of technology to tackle the most intricate issues, ensuring that you achieve triumph in your endeavors. We are your trusted partner in achieving technological victories that drive progress and achieve your objectives</p>
 
@@ -16,7 +16,7 @@
             <div class="bg-[#dc2828] w-[100px] h-[5px] my-4"></div>
         </div>
 
-        <div class="rounded-[12px] border border-[2px] border-gray-400 flex items-center space-x-5 px-8 h-[70px] anim">
+        <div class="sm:rounded-[12px] rounded-[5px] border border-[2px] border-gray-400 flex items-center space-x-5 px-8 sm:h-[70px] anim">
             <div class="first">
                 <img src="../../assets/images/envaccordLogo.png" alt="company's image" class="h-[80%] w-[110px]">
             </div>
